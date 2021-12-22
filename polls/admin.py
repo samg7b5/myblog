fd5eade93c8be70857600097cdb4f85d95102db0
samg7b5 <51542511+samg7b5@users.noreply.github.com>
2021-12-22 18:57:20 +0000

    Customise admin forms
    Includes admin.display decorator to enable sort by arbitrary method

--- polls/admin.py
+++ polls/admin.py
@@ -10,16 +10,18 @@ class ChoiceInline(admin.TabularInline):
 #  create a model admin class, then pass it as the 
 #  second argument to admin.site.register()
 class QuestionAdmin(admin.ModelAdmin):
-    
-    # defines Question overview
+
+	# defines Question overview
 	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
 
 	# defines drilldown
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
 
 
 
