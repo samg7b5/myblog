8e07095146984d69cf368eaf30658e11f96964b1
samg7b5 <51542511+samg7b5@users.noreply.github.com>
2021-12-22 18:44:20 +0000

    Update admin.py
    Customise field layout (admin form)

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from .models import Choice, Question
 
 admin.site.register(Choice)
-admin.site.register(Question)
+
+#  to customise an admin form (e.g. layout fields) 
+#  create a model admin class, then pass it as the 
+#  second argument to admin.site.register()
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
