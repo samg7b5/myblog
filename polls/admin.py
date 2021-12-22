d17ef4cbd7dd6683feb3f8db3f22d19a1dc7d479
samg7b5 <51542511+samg7b5@users.noreply.github.com>
2021-12-22 18:53:32 +0000

    Update admin.py
    Customise admin layout

--- polls/admin.py
+++ polls/admin.py
@@ -2,15 +2,25 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 #  to customise an admin form (e.g. layout fields) 
 #  create a model admin class, then pass it as the 
 #  second argument to admin.site.register()
 class QuestionAdmin(admin.ModelAdmin):
+    
+    # defines Question overview
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+	# defines drilldown
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+
+
 
 admin.site.register(Question, QuestionAdmin)
